0bd8655536a60d5960cca6c9937332c63b719d2d
shivagr29 <shivani.agrawal@wissen.com>
2022-02-17 15:28:53 +0530

    xyz.java created

--- Abc.java
+++ Abc.java
@@ -2,6 +2,10 @@ public Class Abc{
 
 public static void main(String args[])
 {
+
+for(int i=0;i<10;i++)
+{
 System.out.println("hello world!");
+}
 }
 }
---
+++ xyz.java
@@ -0,0 +1,11 @@
+public class xyz{
+   private int x;
+   private String str;
+
+   public void setstr(){
+   }
+   public void getstr(){
+      return str;
+   }
+  
+}
