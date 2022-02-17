d854844c9fb58d4e002617473582075502fdf986
shivagr29 <shivani.agrawal@wissen.com>
2022-02-17 15:35:06 +0530

    changes are done

--- Abc.java
+++ Abc.java
@@ -5,7 +5,8 @@ public static void main(String args[])
 
 for(int i=0;i<10;i++)
 {
-System.out.println("hello world!");
+System.out.println("Pi value" + Math.PI);
 }
+System.out.println("Thank you");
 }
 }
---
+++ az.java
@@ -0,0 +1,3 @@
+public interface az{
+ void some();
+}
